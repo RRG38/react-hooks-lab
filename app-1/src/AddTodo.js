c4c4734cf50c2175210fb5ca34532a5e1f1adc37
Ryan Garren <rryangarren@gmail.com>
2021-03-30 16:56:39 -0600

    completed basic functionality for app-1

---
+++ app-1/src/AddTodo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function AddTodo() {
+  return(
+    <div>
+      <input
+        placeholder='Add a todo!'
+      />
+      <button>Submit</button>
+    </div>
+  );
+};
+
+export default AddTodo;
---
+++ app-1/src/Todo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Todo = (props) => {
+  return(
+    <h2>Test</h2>
+  )
+}
+
+export default Todo;
